0d0c86a47716ad762059b7a5d62291e4453465c4
Wybie <lyrncofyn@gmail.com>
2025-11-29 12:09:58 +0800

    add OAuth routes for provider authentication

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\NotificationController;
+use App\Http\Controllers\Api\OauthController;
 use App\Http\Controllers\Api\PondController;
 use App\Http\Controllers\Api\ScheduleController;
 use Illuminate\Http\Request;
@@ -10,6 +11,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+
+Route::get('/auth/{provider}/redirect', [OauthController::class, 'redirectToProvider']);
+Route::get('/auth/{provider}/callback', [OauthController::class, 'handleProviderCallback']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
