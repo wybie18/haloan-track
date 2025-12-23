6bc9e1a54b7caa0a44f13dfdbabb686ee88cae40
Wybie <lyrncofyn@gmail.com>
2025-12-24 02:43:17 +0800

    build to prod

---
+++ public/build/assets/ApplicationLogo-B5WwY2mt.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-CK1J5-0I.js";function r(o){return a.jsx("img",{src:"/haloan-logo.png",alt:"Haloan Track Logo",...o})}export{r as A};
--- public/build/assets/ApplicationLogo-D4uhoicz.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./app-CWrhXAoG.js";function r(o){return a.jsx("img",{src:"/haloan-logo.png",alt:"Haloan Track Logo",...o})}export{r as A};
